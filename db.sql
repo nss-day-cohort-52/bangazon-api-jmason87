7f91468ee9c46206185b3a63c56f2933d579e298
jmason87 <joewebdev87@gmail.com>
2022-03-03 10:32:27 -0600

    incomplete orders report

--- db.sql
+++ db.sql
@@ -11,14 +11,19 @@ ON op.product_id = p.id
 GROUP BY o.id
 
 
-SELECT o.id order_id, u.first_name, u.last_name, pt.merchant_name payment_type, op.*, SUM(p.price) total_price
+SELECT o.id as order_id, u.first_name || " " || u.last_name as full_name, sum(price) as total_paid, pt.merchant_name as payment_type, o.created_on
 FROM bangazon_api_order o
 JOIN auth_user u
 ON o.user_id = u.id
-JOIN bangazon_api_paymenttype pt 
-ON o.payment_type_id = pt.id
+LEFT JOIN bangazon_api_paymenttype pt 
+ON pt.id = o.payment_type_id
 JOIN bangazon_api_orderproduct op 
-ON o.id = op.order_id
+ON op.order_id = o.id
 JOIN bangazon_api_product p 
-ON op.product_id = p.id
-GROUP BY o.id
+ON p.id = op.product_id
+WHERE payment_type is NULL
+GROUP BY order_id
+ORDER BY o.created_on ASC
+
+
+
